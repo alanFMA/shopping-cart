b71ba7d15d3b017a892c3eba188456a74e698b53
AlanFMA <alanmonteiro105@gmail.com>
2025-04-29 17:44:15 -0300

    ajuste no css e mock de produtos

--- src/app/products/products.mock.ts
+++ src/app/products/products.mock.ts
@@ -2,4 +2,6 @@ export const PRODUCTS = [
   { id: 1, name: 'Mouse Gamer', price: 120.5 },
   { id: 2, name: 'Teclado Mecânico', price: 230.0 },
   { id: 3, name: 'Monitor 24"', price: 899.99 },
+  { id: 4, name: 'Fone', price: 179.99 },
+  { id: 5, name: 'Mousepad Gamer', price: 56.99 },
 ];
