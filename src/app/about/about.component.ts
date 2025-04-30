69d2352c997e6b45f12b5bf131331d544d42bafd
AlanFMA <alanmonteiro105@gmail.com>
2025-04-30 13:58:16 -0300

    feat: lazy loading antigo e ajustes

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  template: `<h2>Sobre esta aplicação</h2>
+    <p>Projeto Angular com Lazy Loading</p>`,
+})
+export class AboutComponent {}
