e6604dff04d09145ec564052ab3a967a93f3e7e1
AlanFMA <alanmonteiro105@gmail.com>
2025-04-30 14:53:39 -0300

    feat: subscription

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CartService } from '../../core/cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cart-view',
   templateUrl: './cart-view.component.html',
   styleUrls: ['./cart-view.component.scss'],
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit, OnDestroy {
   items$ = this.cartService.items$;
   loading$ = this.cartService.loading$;
   checkoutSuccess$ = this.cartService.checkoutSuccess$;
 
+  private subscriptions: Subscription[] = [];
+
   constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.subscriptions.push(
+      this.cartService.items$.subscribe((items) => {
+        console.log('Itens (sub 1):', items.length);
+      })
+    );
+
+    this.subscriptions.push(
+      this.cartService.items$.subscribe((items) => {
+        console.log('Itens (sub 2):', items.map((i) => i.name).join(', '));
+      })
+    );
+
+    this.subscriptions.push(
+      this.cartService.checkoutSuccess$.subscribe((success) => {
+        if (success) {
+          alert('Compra realizada com sucesso (do subscribe em ngOnInit)!');
+        }
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
 
   getTotal() {
     return this.cartService.getTotal();
@@ -23,5 +52,11 @@ export class CartViewComponent {
 
   checkout() {
     this.cartService.checkout();
+
+    this.cartService.checkoutSuccess$.subscribe((success) => {
+      if (success) {
+        alert('Compra finalizada com alerta duplicado!');
+      }
+    });
   }
 }
